8165c6daf26cbe32e45830b2da87be41592d91a9
Dr. Hoss <video@DESKTOP-CDM3URT>
2019-05-15 18:09:52 -0400

    Added tools to change the contents of the Inventory object.

--- src/com/dcreeperstone/Inventory.java
+++ src/com/dcreeperstone/Inventory.java
@@ -17,6 +17,7 @@
  */
 package com.dcreeperstone;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
@@ -29,33 +30,47 @@ import java.util.Scanner;
  * @author Dr. Hoss
  */
 public class Inventory {
-    private final String name;
-    private final HashSet<Item> inventory;
-    
+
+    private String name;
+    private HashSet<Item> inventory;
+
     private static final int MAX_LINE_LENGTH = 80;
-    
+
     /**
-     * 
-     * @param inName 
+     *
+     * @param inName
      */
     public Inventory(String inName) {
         inventory = new HashSet<>();
         name = inName;
         logChanges("'" + inName + "' inventory created.");
     }
-    
-    public Inventory(String inName, Scanner invSC) {
-        inventory = new HashSet<>();
-        name = inName;
-        logChanges("'" + inName + "' inventory created.");
-        loadInventory(invSC);
+
+    /**
+     * 
+     * @param invFile
+     */
+    public Inventory(File invFile) {
+        try {
+            String invName;
+            Scanner invSC = new Scanner(invFile);
+            
+            invName = invSC.nextLine();
+            inventory = new HashSet<>();
+            name = invName;
+            logChanges("'" + invName + "' inventory loaded from '" 
+                    + invFile.getPath() + "'.");
+            loadInventory(invSC);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
-    
+
     private void loadInventory(Scanner invSC) {
         Item tempItem;
         String tempInfo;
         int tempQty;
-        
+
         while (invSC.hasNext()) {
             tempInfo = invSC.nextLine();
             tempQty = Integer.parseInt(invSC.nextLine());
@@ -63,198 +78,222 @@ public class Inventory {
             addNewItem(tempItem);
         }
     }
-    
+
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     public String getName() {
         return name;
     }
-    
+
     /**
-     * 
-     * @param inItem 
+     *
+     * @param inItem
      */
     public void addNewItem(Item inItem) {
         Item searchItem;
-        Iterator<Item> it = inventory.iterator();
-        boolean itemFound = false;
-        while (it.hasNext()) {
-            searchItem = it.next();
-            if (searchItem.getInfo().equalsIgnoreCase(inItem.getInfo())) {
-                inventory.forEach(item -> {
-                    if (item.getInfo().equalsIgnoreCase(inItem.getInfo()))
-                        item.addToQuantity(1);
-                });
-                itemFound = true;
-                logChanges("Added 1 to item '" + inItem.getInfo() 
+        
+        searchItem = search(inItem.getInfo());
+        if (searchItem != null)
+            inventory.forEach(item -> {
+                if (item.getInfo().equalsIgnoreCase(searchItem.getInfo())) {
+                    item.addToQuantity(inItem.getQuantity());
+                    logChanges("Added " + inItem.getQuantity() + " to item '"
+                        + inItem.getInfo()
                         + "' to inventory due to prior existance.");
-            }
-        }
-        if (!itemFound) {
+                }
+            });
+        else {
             inventory.add(inItem);
-            logChanges("Add item '" + inItem.getInfo() + "' with quantity " 
+            logChanges("Added item '" + inItem.getInfo() + "' with quantity "
                     + inItem.getQuantity() + " to inventory.");
         }
     }
-    
+
     /**
-     * 
+     *
      * @param key
-     * @param amount 
+     * @param amount
      */
     public void addToItem(String key, int amount) {
         Item searchItem;
-        boolean itemFound;
-        
-        itemFound = false;
-        Iterator<Item> it = inventory.iterator();
-        while (it.hasNext() && !itemFound) {
-            searchItem = it.next();
-            if (searchItem.getInfo().equalsIgnoreCase(key)) {
-                inventory.forEach(item -> {
-                    if (item.getInfo().equalsIgnoreCase(key))
-                        item.addToQuantity(amount);
-                });
-                itemFound = true;
-                logChanges("Attempted to add " + amount 
-                        + " to quantity of item '" + searchItem.getInfo() 
-                        + "'.");
-            }
-        }
-        if (!itemFound)
+
+        searchItem = search(key);
+        if (searchItem != null) {
+            inventory.forEach(item -> {
+                if (item.getInfo().equalsIgnoreCase(key))
+                    item.addToQuantity(amount);
+            });
+            logChanges("Attempted to add " + amount
+                    + " to quantity of item '" + searchItem.getInfo()
+                    + "'.");
+        } else
             System.out.println("Item could not be found in the inventory.");
     }
-    
+
     /**
-     * 
-     * @param key 
+     *
+     * @param key
      */
     public void removeItem(String key) {
         Item searchItem;
-        boolean itemFound;
-        
-        itemFound = false;
-        Iterator<Item> it = inventory.iterator();
-        while (it.hasNext() && !itemFound) {
-            searchItem = it.next();
-            if (searchItem.getInfo().equalsIgnoreCase(key)) {
-                inventory.remove(searchItem);
-                itemFound = true;
-                logChanges("Removed item '" + searchItem.getInfo() 
-                        + "' from inventory.");
-            }
-        }
-        if (!itemFound)
+
+        searchItem = search(key);
+        if (searchItem != null) {
+            inventory.remove(searchItem);
+            logChanges("Removed item '" + searchItem.getInfo()
+                    + "' from inventory.");
+        } else {
             System.out.println("Item could not be found in the inventory.");
+        }
     }
-    
+
     /**
-     * 
+     *
      * @param key
-     * @param amount 
+     * @param amount
      */
     public void removeFromItem(String key, int amount) {
         Item searchItem;
-        boolean itemFound;
         
-        itemFound = false;
-        Iterator<Item> it = inventory.iterator();
-        while (it.hasNext() && !itemFound) {
-            searchItem = it.next();
-            if (searchItem.getInfo().equalsIgnoreCase(key)) {
-                inventory.forEach(item -> {
-                    if (item.getInfo().equalsIgnoreCase(key))
-                        item.removeFromQuantity(amount);
-                });
-                itemFound = true;
-                logChanges("Attempted to remove " + amount 
-                        + " from quantity of item '" + searchItem.getInfo() 
+        searchItem = search(key);
+        if (searchItem != null)
+            inventory.forEach(item -> {
+                if (item.getInfo().equalsIgnoreCase(searchItem.getInfo())) {
+                    item.removeFromQuantity(amount);
+                    logChanges("Attempted to remove " + amount
+                        + " from quantity of item '" + searchItem.getInfo()
                         + "'.");
-            }
-        }
-        if (!itemFound)
+                }
+            });
+        else
             System.out.println("Item could not be found in the inventory.");
     }
-    
+
     /**
      * 
-     * @param key 
+     * @param key
+     * @return 
+     */
+    public Item search(String key) {
+        Item searchItem = null;
+        boolean itemFound = false;
+        Iterator<Item> it = inventory.iterator();
+
+        while (it.hasNext() && !itemFound) {
+            searchItem = it.next();
+            if (searchItem.getInfo().equalsIgnoreCase(key))
+                itemFound = true;
+            else
+                searchItem = null;
+        }
+
+        return searchItem;
+    }
+
+    /**
+     *
+     * @param key
      */
     public void resetItem(String key) {
         Item searchItem;
-        boolean itemFound;
         
-        itemFound = false;
-        Iterator<Item> it = inventory.iterator();
-        while (it.hasNext() && !itemFound) {
-            searchItem = it.next();
-            if (searchItem.getInfo().equalsIgnoreCase(key)) {
-                inventory.forEach(item -> {
-                    if (item.getInfo().equalsIgnoreCase(key))
-                        item.resetQuantity();
-                });
-                logChanges("Reset item '" + searchItem.getInfo() 
+        searchItem = search(key);
+        if (searchItem != null)
+            inventory.forEach(item -> {
+                if (item.getInfo().equalsIgnoreCase(searchItem.getInfo())) {
+                    item.resetQuantity();
+                    logChanges("Reset item '" + searchItem.getInfo()
                         + "' quantity to 0.");
-                itemFound = true;
-            }
-        }
-        if (!itemFound)
+                }
+            });
+        else
             System.out.println("Item could not be found in the inventory.");
     }
-    
+
     /**
-     * 
+     *
      */
     public void resetAllItems() {
         inventory.forEach(Item::resetQuantity);
         logChanges("Reset all item quantites to 0.");
     }
-    
+
     /**
-     * 
+     *
      */
     public void clearInventory() {
         inventory.clear();
         logChanges("Removed all items from '" + name + "' inventory.");
     }
-    
+
     /**
-     * 
+     *
      */
     public void displayAllItems() {
         Iterator<Item> it = inventory.iterator();
         Item currItem;
-        
+
         if (it.hasNext()) {
-            for (int i = 0; i < (MAX_LINE_LENGTH - name.length()) / 2; i++)
+            for (int i = 0; i < (MAX_LINE_LENGTH - name.length()) / 2; i++) {
                 System.out.print("=");
+            }
             System.out.print(name);
-            if (name.length() % 2 == 0)
+            if (name.length() % 2 == 0) {
                 for (int i = 0; i < (MAX_LINE_LENGTH - name.length()) / 2; i++)
                     System.out.print("=");
-            else
-                for (int i = 0; i < (MAX_LINE_LENGTH - name.length()) / 2 + 1; 
-                        i++)
+            } else {
+                for (int i = 0; i < (MAX_LINE_LENGTH - name.length()) / 2 + 1;
+                        i++) {
                     System.out.print("=");
+                }
+            }
             System.out.println();
             System.out.format("%-40s%40s\n", "ITEM", "QUANTITY");
-            for (int i = 0; i < MAX_LINE_LENGTH; i++)
+            for (int i = 0; i < MAX_LINE_LENGTH; i++) {
                 System.out.print("-");
+            }
             System.out.println();
             while (it.hasNext()) {
                 currItem = it.next();
-                System.out.format("%-40s%40d\n", currItem.getInfo(), 
+                System.out.format("%-40s%40d\n", currItem.getInfo(),
                         currItem.getQuantity());
             }
-        }
+        } 
         else
             System.out.println("There are currently no items in the "
                     + "inventory.");
         System.out.println();
     }
-    
+
+    /**
+     * 
+     * @param path 
+     */
+    public void saveDataTo(String directory, String fileName) {
+        PrintWriter invPW;
+        String path;
+        
+        path = directory + "/" + fileName;
+        
+        try {
+            File file = new File(directory);
+            if (!file.exists())
+                file.mkdir();
+            invPW = new PrintWriter(path);
+            invPW.println(name);
+            inventory.forEach(item -> {
+                invPW.println(item.getInfo());
+                invPW.println(item.getQuantity());
+            });
+            invPW.close();
+            logChanges("Inventory saved to '" + path + "'.");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
     private void logChanges(String message) {
         FileWriter logFile;
         PrintWriter logPW;
